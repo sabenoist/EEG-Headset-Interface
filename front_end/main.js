6dc6970d4a54c773aaf982cd4b68552fa6abd16a
Sander Arjan Benoist <sabenoist5@gmail.com>
2018-08-07 22:09:37 +0200

    replaced jquery GET request with standard javascript one

--- front_end/main.js
+++ front_end/main.js
@@ -1,9 +1,15 @@
 function start_streaming() {
-	$.get("127.0.0.1:5000/start_streaming")
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", "127.0.0.1:5000/start_streaming", false);
+	xmlHttp.send(null);
+
 	window.alert("started streaming!")
 }
 
 function stop_streaming() {
-	$.get("127.0.0.1:5000/stop_streaming")
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", "127.0.0.1:5000/stop_streaming", false);
+	xmlHttp.send(null);
+
 	window.alert("stopped streaming!")
 }
